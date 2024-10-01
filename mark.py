21c7b9edb3aaed87c5f35e9f5746d206206f69ee
vaishnavi773 <155604199+vaishnavi773@users.noreply.github.com>
2024-10-01 07:53:48 +0100

    Add files via upload

---
+++ mark.py
@@ -0,0 +1,11 @@
+s1=int(input("enter a mark:"))
+s2=int(input("enter a mark:"))
+s3=int(input("enter a mark:"))
+s4=int(input("enter a mark:"))
+s5=int(input("enter a mark:"))
+mark=int(input("enter the mark of all subjects:"))
+total=s1+s2+s3+s4+s5
+print(total)
+percentage=(total/mark)*100
+print(percentage)
+              
