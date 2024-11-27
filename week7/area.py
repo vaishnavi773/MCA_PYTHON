eb7968141a2668eb139caec9d3a0ae553053ce03
vaishnavi773 <155604199+vaishnavi773@users.noreply.github.com>
2024-11-27 06:48:05 +0000

    Add files via upload

---
+++ week7/area.py
@@ -0,0 +1,7 @@
+def rectangle_area(length,width):
+    return length*width
+def circle_area(radius):
+    from math import pi
+    return pi*radius*radius
+def triangle_area(base,height):
+    return 0.5*base*height
