deb9ceacef0e581196ed3591180c847431a19d41
vaishnavi773 <155604199+vaishnavi773@users.noreply.github.com>
2024-11-29 09:46:17 +0000

    Delete week7/area.py

--- week7/area.py
+++
@@ -1,7 +0,0 @@
-def rectangle_area(length,width):
-    return length*width
-def circle_area(radius):
-    from math import pi
-    return pi*radius*radius
-def triangle_area(base,height):
-    return 0.5*base*height
