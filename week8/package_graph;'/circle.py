8983ea2087829e41936ebcb5939288b3e672ee48
vaishnavi773 <155604199+vaishnavi773@users.noreply.github.com>
2024-12-18 08:51:58 +0000

    Add files via upload

---
+++ week8/package_graph;'/circle.py
@@ -0,0 +1,6 @@
+import math
+def area_circle(r): 
+    return math.pi *r**2
+def perimeter(r):
+    return 2*math.pi*r
+
---
+++ week8/package_graph;'/rectangle.py
@@ -0,0 +1,4 @@
+def rect_area(l,w):
+  return l*w
+def rect_perimeter(l,w):
+    return 2*(l+w)
